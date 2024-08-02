df8b7b5c71a61208ad2db27548382a916ad4ea17
CelineTrammi <celinetram@gmail.com>
2024-08-02 14:52:21 +0200

    Some comments to describe different plugins used

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,32 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
 vim.g.have_nerd_font = true
 
 require("config.launch")
 require("config.options")
 require("config.keymap")
 
+-- show keys with description
 spec("plugins.which-key")
+-- git hunks for staging and previewing
 spec("plugins.gitsigns")
+-- telescope, fuzzy find, etc.
 spec("plugins.telescope")
+-- language server protocol (lsp)
 spec("plugins.lspconfig")
+-- formatter
 spec("plugins.conform")
+-- autocompletion
 spec("plugins.cmp")
+-- theme
 spec("plugins.theme.tokyonight")
+-- mini files, mini ai, mini surround
 spec("plugins.mini")
+-- indent guides
 spec("plugins.indent_line")
+-- linting
 spec("plugins.lint")
+-- automatically pair brackets together
 spec("plugins.autopairs")
-spec("plugins.neo-tree")
+
 require("config.lazy-bootstrap")
